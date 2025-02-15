2aca3bd20341c023764707e24698efabea39b348
pankaj <imvpankaj@gmail.com>
2025-02-15 18:34:36 +0530

    qwe

--- src/mini component/MouseScroll.tsx
+++ src/mini component/MouseScroll.tsx
@@ -4,9 +4,9 @@ const MouseScroll = () => {
   return (
     <div>
     
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce z-10">
         <div className="w-8 h-12 rounded-full border-2 border-white flex items-start justify-center p-2">
-          <div className="w-1 h-3 bg-white rounded-full animate-scroll" />
+          <div className="w-1 h-3 bg-white rounded-full animate-scroll z-10" >|</div>
         </div>
       </div>
     </div>
